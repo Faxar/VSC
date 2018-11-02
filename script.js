77f7e17e0a9801ec1983780fb885a5785e5488cc
Faxar <alazerus@gmail.com>
2018-11-02 17:12:54 +0200

    J 2nd Update

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
+// Check if local storage exist.
+// If "Yes", load previous search.
+(()=> {
+    if(localStorage.getItem('boxes')){
+        $('.searchResults').html(window.localStorage.getItem('boxes'));
+    }
+})();
+
+// Event listener attached to search button.
 document.getElementById("btn1").addEventListener("click", () => {
-    get(document.getElementById("searchField").value);
+    var searchText = document.getElementById("searchField").value;
+    if(searchText !== ''){
+        window.localStorage.removeItem('boxes')
+        get(searchText);
+    } 
 });
 
-$('')
 
+// Requst API for searched value.
 function get(searchText){
     axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+searchText)
     .then((response) => {
         console.log(response);
-        if(response.data.drinks.length > 0){
             let drinks = response.data.drinks;
             let output = '';
-            for(var i = 0; i< drinks.length; i++){
-                console.log(drinks[i].strDrink);
+            $.each(drinks, (id, value) => {
                 output += `
-                <div class="box">
-                    <img src="${drinks[i].strDrinkThumb}">
-                    <h4>${drinks[i].strDrink}</h4>
-                </div>`
-            }
+                <div class="box" id="${value.idDrink}">
+                    <img src="${value.strDrinkThumb}">
+                    <h4>${value.strDrink}</h4>
+                </div>`   
+            })
         $('#ent').html(output);
-        }
-        
+        var html = $('.searchResults').html();
+        window.localStorage.setItem('boxes', html);       
     })
     .catch((error) => {
         console.log(error)
     })
 }
+
+
+// Change page and store search result on the page.
+$(document).on('click', ".box", function() {
+    var attr = $(this).attr('id');
+    // var drinkObj = drinks.find(function(e){
+    //     return e.idDrink === attr;
+    // });
+    sessionStorage.setItem('id', attr);
+    document.location.href = "http://127.0.0.1:5500/details.html"
+})
